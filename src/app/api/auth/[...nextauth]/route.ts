2d26bd85a79c9b27928bb638c1429217842a7401
Albert Genis evangelista <albertgenisevangelista@Alberts-MacBook-Air.local>
2023-07-10 19:24:27 +0200

    feat(profile): profile interface with saved plans

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,51 +1,9 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import options from "./options";
 import { signIn } from "next-auth/react";
-import prisma from "../../../../../db";
 
 // Initialize NextAuth
 
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
-    }),
-  ],
-
-  callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      try {
-        // console.log("USER", user);
-        // console.log("ACOUNT", account);
-        // console.log("PROFILE", profile);
-        // console.log("EMAIL", email);
-        // console.log("CREDENTIALS", credentials);
-        const oldUser = await prisma.user.findUnique({
-          where: {
-            id: user.id,
-          },
-        });
-        if (oldUser) {
-          return true;
-        }
-        if (user.id && user.name && user.email) {
-          const newUser = await prisma.user.create({
-            data: {
-              id: user.id,
-              username: user.name,
-              email: user.email,
-            },
-          });
-          return true;
-        }
-        return false;
-      } catch (e) {
-        console.log(e);
-        return false;
-      }
-    },
-  },
-});
+const handler = NextAuth(options);
 
 export { handler as GET, handler as POST };
--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -6,7 +6,10 @@ import Map from "../components/Map";
 import ExactLocation from "../components/ExactLocation";
 import ChangeMeBtn from "../components/ChangeMeBtn";
 import { useSession } from "next-auth/react";
-import { useSearchParams } from "next/navigation";
+import { redirect, useSearchParams } from "next/navigation";
+import { RedirectType } from "next/dist/client/components/redirect";
+import Link from "next/link";
+import SavePlanButton from "../components/SavePlanButton";
 
 function ResultsPage() {
   const [result, setResult] = useState<ResultData>([]);
@@ -53,26 +56,7 @@ function ResultsPage() {
     <div className="flex h-screen w-screen ">
       <div className="overflow-auto p-6 bg-white border border-gray-200 shadow">
         <div className="flex justify-end w-full p-2 ">
-          <button
-            onClick={handleSave}
-            className="flex p-2 w-fit rounded-xl bg-yellow-400 text-white gap-2"
-          >
-            Save{" "}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
-              />
-            </svg>
-          </button>
+          <SavePlanButton handleSave={handleSave} />
         </div>
         {result.map((dataResponse, i) => {
           return (
